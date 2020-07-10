9f4328f344c8493ba35161c7fb491491a0cb409c
lukedavidcowley <lukedavidcowley@gmail.com>
2020-07-10 12:15:06 -0400

    service tests scaffold

---
+++ LukeCowley.Web/LukeCowley.Business.Tests/Services/MarsWeatherService.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LukeCowley.Business.Tests.Services
+{
+    public class MarsWeatherServiceTests
+    {
+        [Test]
+        public Task GetWeatherProfileAsync_ReturnsEmptyIEnumerableWithEmptyRepo()
+        {
+            //assemble
+
+            //act
+
+            //assert
+
+            throw new NotImplementedException();
+        }
+        [Test]
+        public Task GetWeatherProfileAsync_ReturnsResultsIfRepositoryIsNotEmpty()
+        {
+            //assemble
+
+            //act
+            
+            //assert
+
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public Task GetWeatherProfileAsync_ReturnsCorrectResults()
+        {
+            //assemble
+
+            //act
+
+            //assert
+
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public Task UpdateWeatherAsync_CorrectlyUpdatesRepositort()
+        {
+            //assemble
+
+            //act
+
+            //assert
+
+            throw new NotImplementedException();
+        }
+    }
+}
--- LukeCowley.Web/LukeCowley.Business/Data/Providers/IMarsWeatherDataProvider.cs
+++ LukeCowley.Web/LukeCowley.Business/Data/Providers/IMarsWeatherDataProvider.cs
@@ -1,7 +1,5 @@
 ﻿using LukeCowley.Business.Models;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace LukeCowley.Business.Data.Providers
